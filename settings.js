d92edb68ff3637097f1a248f216979afea3ba571
usman640402 <bothosting7@gmail.com>
2025-02-11 19:00:47 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
 //=================================================//
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'XPLOADER-BOT:~qqxxgKwJ#dc05Lip1VtXaUy24qlbXWV8aokSLHcvBfzqiCmcxjAs' 
 //Enter your Xploader session id here; must start with XPLOADER-BOT:~
 
 //=================================================//
